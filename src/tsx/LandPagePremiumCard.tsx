77ec257eae23493580f207324a906ce43f88d000
Fabrice RAKOTOMAMPIANDRA <fabbertrant@yahoo.fr>
2023-08-11 23:14:04 +0300

    Version Preview 3

--- src/tsx/LandPagePremiumCard.tsx
+++ src/tsx/LandPagePremiumCard.tsx
@@ -2,8 +2,8 @@ var LandPagePremiumCard = function() {
 	return <div className="landPagePremiumCard">
 		<img className="landPagePremiumCardLeft" src="./icon/premium.svg" alt="premium" />
 		<div className="landPagePremiumCardRight">
-			<h5>Premimum Account</h5>
-			<p>You have a premium account, granting you access to all the remarkable features offered by RsumeDone. With this privilege, you can indulge in the freedom of downloading an unlimed number of resumes and cover letters in both PDF and Word formats.</p>
+			<h5>Premium Account</h5>
+			<p>You have a premium account, granting you access to all the remarkable features offered by ResumeDone. With this privilege, you can indulge in the freedom of downloading an unlimed number of resumes and cover letters in both PDF and Word formats.</p>
 		</div>
 	</div>;
 };
